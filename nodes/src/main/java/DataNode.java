5fd36d2400bf19540c3f798737befcda11399d69
arkbriar <arkbriar@gmail.com>
2016-12-10 15:51:15 +0800

    initial commit

---
+++ nodes/src/main/java/DataNode.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Shunjie Ding on 10/12/2016.
+ */
+public class DataNode {
+    public static void main(String... args) {}
+}
---
+++ nodes/src/main/java/MasterNode.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Shunjie Ding on 10/12/2016.
+ */
+public class MasterNode {
+    public static void main(String... args) {}
+}
