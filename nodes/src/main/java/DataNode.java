bbc03f471e2fe0d9dc077b131a8fac8ad9b86ee6
arkbriar <arkbriar@gmail.com>
2016-12-12 21:38:56 +0800

    use apache.common to parse command line arguments

--- nodes/src/main/java/DataNode.java
+++ nodes/src/main/java/DataNode.java
@@ -1,3 +1,9 @@
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -8,12 +14,18 @@ import java.util.logging.Logger;
 public class DataNode {
     private static final Logger logger = Logger.getLogger(DataNode.class.getName());
 
-    public static void main(String... args) throws IOException, InterruptedException {
-        if (args.length == 0) {
-            logger.log(Level.WARNING, "First param should be a port number.");
-            System.exit(1);
+    public static void main(String... args)
+        throws IOException, InterruptedException, ParseException {
+        Options options = new Options();
+        options.addOption("p", "port", true, "Port of the service");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        int port = 10088;
+        if (cmd.hasOption("t")) {
+            port = Integer.valueOf(cmd.getOptionValue("t"));
+        } else {
+            logger.log(Level.WARNING, "No port specified, using default port 8088.");
         }
-        int port = Integer.valueOf(args[0]);
         DataNodeServer server = new DataNodeServer(port);
         server.startAndBlock();
     }
--- nodes/src/main/java/MasterNode.java
+++ nodes/src/main/java/MasterNode.java
@@ -1,3 +1,9 @@
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -8,12 +14,17 @@ import java.util.logging.Logger;
 public class MasterNode {
     private static final Logger logger = Logger.getLogger(MasterNode.class.getName());
 
-    public static void main(String... args) throws IOException, InterruptedException {
+    public static void main(String... args)
+        throws IOException, InterruptedException, ParseException {
+        Options options = new Options();
+        options.addOption("p", "port", true, "Port of the service");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
         int port = 8088;
-        if (args.length == 0) {
-            logger.log(Level.WARNING, "No port specified, using default port 8088.");
+        if (cmd.hasOption("t")) {
+            port = Integer.valueOf(cmd.getOptionValue("t"));
         } else {
-            port = Integer.valueOf(args[0]);
+            logger.log(Level.WARNING, "No port specified, using default port 8088.");
         }
         MasterNodeServer server = new MasterNodeServer(port);
         server.startAndBlock();
